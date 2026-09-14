a136b854fd354d6a60bc4b81bbbc78da0602d751
Alessandro-Milani <milani.alessandro.09@itisdalmine.edu.it>
2026-09-14 13:16:36 +0200

    Prima commit

---
+++ Prova/src/Main.java
@@ -0,0 +1,25 @@
+void main() {
+    String nome = "Mario";
+    int numero = 123;
+    boolean finito = false;
+    float vel = 1.2345f;
+
+    IO.println(numero*873);
+    IO.println(numero/6);
+    IO.println(numero+73);
+    IO.println(numero-1);
+    IO.println(numero%2); //modulo:resto della divisione intera
+
+    if(finito){ // mota si scrive !, and si scirve &&
+
+        IO.println("la tua serie preferita è terminata");
+    }
+    else{
+        IO.println("la tua serie preferita non è ancora finita");
+
+    }
+
+
+    IO.println("lol");
+    IO.println(nome);
+}
